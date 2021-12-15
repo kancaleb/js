c369f2bac3dd35bf9aba9fd4d56458a19b903da2
kancaleb <86612782+kancaleb@users.noreply.github.com>
2021-12-15 08:10:12 +0800

    Delete WheresMyPen directory

--- WheresMyPen/assets/Interiors48x48.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Interiors48x48" tilewidth="48" tileheight="48" tilecount="1424" columns="16">
- <image source="Interiors48x48.png" width="768" height="4272"/>
-</tileset>
